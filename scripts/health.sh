ecfc84118fc8f42de3dfc85d431c37a277c44911
SoftDolphin <hawrang17@naver.com>
2022-01-15 21:31:54 +0900

    무중단 배포 스크립트 수정3 로그 추가

--- scripts/health.sh
+++ scripts/health.sh
@@ -17,6 +17,8 @@ do
   RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
   UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 
+  echo "> UP_COUNT check : ${UP_COUNT}"
+
   if [ ${UP_COUNT} -ge 1 ]
   then
     echo "> Health check 성공"
